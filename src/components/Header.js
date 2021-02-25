0556af45f8656187db4cd7f3b12690bd3e26bb5a
zakariaeNasri <75163725+zakariaeNasri@users.noreply.github.com>
2021-02-25 20:57:23 +0100

    navbar_particles_typedtext

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="Main-info">
+                <h1>Web and desktop Developer</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,14 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+           <a className="navbar-brand" href="#"><img className="logo" src={ndit} alt="logo"/></a>
+           <h3 className="h3">New Digital information technologie</h3>
             <div className="container">
-            <a className="navbar-brand" href="#"><img className="logo" src={ndit} alt="logo"/></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faBars} style={{color:"#bff"}}/>
         </button>
-      
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <h3 className="h3">New Digital information technologie</h3>
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
