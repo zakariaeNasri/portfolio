6a0e802f57a6ff00b022ad6e49631e4ecf50a5ac
zakariaeNasri <75163725+zakariaeNasri@users.noreply.github.com>
2021-02-25 21:03:10 +0100

    Navbar comleted

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Particles from "react-particles-js";
 function App() {
   return (
     <>
+    <Particles
+      params={{
+        particles:{
+          number:{
+            value:30,
+            density:{
+              enable:true,
+              value_area:900
+            }
+          },
+          shape:{
+            type: "circle",
+            stroke:{
+              width:6,
+              color: "#bff"
+            }
+          }
+        }
+      }}
+    />
       <Navbar />
       <Header />
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
            <a className="navbar-brand" href="#"><img className="logo" src={ndit} alt="logo"/></a>
-           <h3 className="h3">New Digital information technologie</h3>
+           
             <div className="container">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
